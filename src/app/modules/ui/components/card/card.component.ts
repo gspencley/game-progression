ed84cadf489af2bfbbd9da8f2995a5d61032919f
gspencley <gspencley@yahoo.com>
2018-10-09 12:11:35 -0400

    Refactored UI elements into components and moved related CSS

---
+++ src/app/modules/ui/components/card/card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ks-card',
+  templateUrl: 'card.component.html',
+  styleUrls: [ './card.component.scss' ]
+})
+
+export class CardComponent {}
